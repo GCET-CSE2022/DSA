5d94237574fbf73436bb999cb074110f2d170c3a
FAB <143941135+FaheemorFAB@users.noreply.github.com>
2024-04-21 11:15:41 +0530

    add array programs

---
+++ Array_Insertion.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Inserts a key in arr[] of given capacity.
+// n is current size of arr[]. This
+// function returns n + 1 if insertion
+// is successful, else n.
+int insertSorted(int arr[], int n, int key, int capacity)
+{
+    // Cannot insert more elements if n is
+    // already more than or equal to capacity
+    if (n >= capacity)
+        return n;
+
+    arr[n] = key;
+
+    return (n + 1);
+}
+
+// Driver Code
+int main()
+{
+    int arr[20] = {12, 16, 20, 40, 50, 70};
+    int capacity = sizeof(arr) / sizeof(arr[0]);
+    int n = 6;
+    int i, key = 26;
+
+    printf("\nBefore Insertion: ");
+    for (i = 0; i < n; i++)
+        printf("%d  ", arr[i]);
+
+    // Inserting key
+    n = insertSorted(arr, n, key, capacity);
+
+    printf("\nAfter Insertion: ");
+    for (i = 0; i < n; i++)
+        printf("%d  ", arr[i]);
+
+    return 0;
+}
